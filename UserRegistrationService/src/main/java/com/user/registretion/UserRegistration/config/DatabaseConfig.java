5d5d1772d32c6cbd4ee7c2963a89a13d2721a8ca
filipe-fff <111322276+filipe-fff@users.noreply.github.com>
2025-07-11 23:40:06 -0300

    criação das configurações de cross e hiraki

---
+++ UserRegistrationService/src/main/java/com/user/registretion/UserRegistration/config/DatabaseConfig.java
@@ -0,0 +1,43 @@
+package com.user.registretion.UserRegistration.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfig {
+    @Value("${spring.datasource.url}")
+    String url;
+
+    @Value("${spring.datasource.username}")
+    String username;
+
+    @Value("${spring.datasource.password}")
+    String password;
+
+    @Value("${spring.datasource.driver-class-name}")
+    String driverClassName;
+
+    @Bean
+    public DataSource hikariDataSource() {
+        HikariConfig hikariConfig = new HikariConfig();
+
+        hikariConfig.setJdbcUrl(this.url);
+        hikariConfig.setUsername(this.username);
+        hikariConfig.setPassword(this.password);
+        hikariConfig.setDriverClassName(this.driverClassName);
+
+        hikariConfig.setPoolName("user-registration-db-pool");
+        hikariConfig.setMaximumPoolSize(20);
+        hikariConfig.setMinimumIdle(1);
+        hikariConfig.setConnectionTimeout(100000);
+        hikariConfig.setMaxLifetime(600000L);
+        hikariConfig.setConnectionTestQuery("SELECT 1");
+
+        return new HikariDataSource(hikariConfig);
+    }
+}
