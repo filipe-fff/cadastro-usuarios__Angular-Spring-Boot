def95388211db9f2b0a3cd9f1b0d7ca1cf6eb950
filipe-fff <111322276+filipe-fff@users.noreply.github.com>
2025-07-13 19:28:02 -0300

    service de usuario

--- userRegistration/src/app/app.component.ts
+++ userRegistration/src/app/app.component.ts
@@ -1,25 +1,13 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { UsersListService } from './services/users-list.service';
-import { Observable, of } from 'rxjs';
-import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet,
-    AsyncPipe
+    RouterOutlet
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
-  usersList$: Observable<any[]> = of([]);
-
-  private readonly _usersListService = inject(UsersListService);
-
-  ngOnInit(): void {
-    this.usersList$ = this._usersListService.getUsers();
-  }
-}
+export class AppComponent { }
--- userRegistration/src/app/services/users-list.service.ts
+++ userRegistration/src/app/services/users-list.service.ts
@@ -8,7 +8,35 @@ import { Observable } from "rxjs";
 export class UsersListService {
     private readonly _httpClient = inject(HttpClient);
 
+    // CREATE
+    save(user: any): Observable<any> {
+        return this._httpClient.post<any>("http://localhost:8081/user-registration/save", { ...user });
+    }
+    
+    // READ
     getUsers(): Observable<any[]> {
         return this._httpClient.get<any[]>("http://localhost:8081/user-registration");
+    }
+
+    getUserById(id: string): Observable<any> {
+        return this._httpClient.get("http://localhost:8081/user-registration/user/" + id);
+    }
+
+    existsName(name: string): Observable<boolean> {
+        return this._httpClient.post<boolean>("http://localhost:8081/user-registration/existsName", name);
+    }
+
+    existsEmail(email: string): Observable<boolean> {
+        return this._httpClient.post<boolean>("http://localhost:8081/user-registration/existsEmail", email);
+    }
+
+    // UPDATE
+    update(id: string, user: any): Observable<any> {
+        return this._httpClient.put<any>("http://localhost:8081/user-registration/update/" + id, user);
+    }
+
+    // DELETE
+    delete(id: string): void {
+        this._httpClient.delete<void>("http://localhost:8081/user-registration/delete/" + id);
     }
 }
