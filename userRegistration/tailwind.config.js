428ea8d92edb488ae2c09ef4a7503d6806be0252
filipe-fff <111322276+filipe-fff@users.noreply.github.com>
2025-07-14 20:32:08 -0300

    instalando e configurando o tailwind e angular material

---
+++ userRegistration/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html, js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// npm i -D tailwindcss postcss autoprefixer
+// npx tailwindcss init
+// @tailwind base;
+// @tailwind components;
+// @tailwind utilities;
