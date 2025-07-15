7e6e900e095941ff1df384f569eb99acc4e5a86a
filipe-fff <111322276+filipe-fff@users.noreply.github.com>
2025-07-15 17:38:29 -0300

    criação do componente de informações de contato

---
+++ userRegistration/src/app/components/contact-informations/contact-informations.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-informations',
+  standalone: true,
+  imports: [],
+  templateUrl: './contact-informations.component.html',
+  styleUrl: './contact-informations.component.scss'
+})
+export class ContactInformationsComponent {
+
+}
--- userRegistration/src/app/components/user-container/user-container.component.ts
+++ userRegistration/src/app/components/user-container/user-container.component.ts
@@ -2,17 +2,21 @@ import { Component, Input } from '@angular/core';
 import { AngularMaterialModule } from '../../angular-material/angular-material.module';
 import { GeneralInformationsComponent } from '../general-informations/general-informations.component';
 import { IUser } from '../../interfaces/user/user.interface';
+import { ContactInformationsComponent } from '../contact-informations/contact-informations.component';
 
 @Component({
   selector: 'app-user-container',
   standalone: true,
   imports: [
     AngularMaterialModule,
-    GeneralInformationsComponent
+    GeneralInformationsComponent,
+    ContactInformationsComponent
   ],
   templateUrl: './user-container.component.html',
   styleUrl: './user-container.component.scss'
 })
 export class UserContainerComponent {
+  currentTabIndex = 1;
+
   @Input({ required: true }) userSelected: IUser = {} as IUser;
 }
