1c64cf4b75dac99d9f2672d07d471ab907feaff1
filipe-fff <111322276+filipe-fff@users.noreply.github.com>
2025-07-14 22:21:19 -0300

    enviando usuarios para a lista de usuarios

--- userRegistration/src/app/app.component.ts
+++ userRegistration/src/app/app.component.ts
@@ -1,24 +1,14 @@
 import { AsyncPipe, JsonPipe } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { GenresService } from './services/genres.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet,
-    AsyncPipe,
-    JsonPipe
+    RouterOutlet
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
-
-  private readonly _genresListService = inject(GenresService);
-
-  ngOnInit() {
-    this._genresListService.getGenres().subscribe(console.log);
-  }
-}
+export class AppComponent { }
--- userRegistration/src/app/components/users-list/users-list.component.ts
+++ userRegistration/src/app/components/users-list/users-list.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { AngularMaterialModule } from '../../angular-material/angular-material.module';
+import { Observable, of } from 'rxjs';
+import { UsersListReponse } from '../../types/users-list-response';
+import { UsersService } from '../../services/users.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-users-list',
   standalone: true,
   imports: [
+    AsyncPipe,
     AngularMaterialModule
   ],
   templateUrl: './users-list.component.html',
   styleUrl: './users-list.component.scss'
 })
-export class UsersListComponent {
+export class UsersListComponent implements OnInit {
+  $usersList: Observable<UsersListReponse> = of([]);
 
+  private readonly _usersService = inject(UsersService);
+
+  ngOnInit() {
+    console.log("ngOnInit");
+    this.$usersList = this._usersService.getUsers();
+  }
 }
