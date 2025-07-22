ee8fa33c7e2fb8825c62fbc1be61393a90bc777b
filipe-fff <111322276+filipe-fff@users.noreply.github.com>
2025-07-22 02:10:25 -0300

    listando a lista de paises e exibindo erro no campo de país

--- userRegistration/src/app/components/general-informations-edit/general-informations-edit.component.ts
+++ userRegistration/src/app/components/general-informations-edit/general-informations-edit.component.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { AngularMaterialModule } from '../../angular-material/angular-material.module';
 import { passwordStrengthProgressBar } from '../../utils/password-strength-progress-bar';
+import { CountriesList } from '../../types/countries-list';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-general-informations-edit',
@@ -15,12 +17,16 @@ import { passwordStrengthProgressBar } from '../../utils/password-strength-progr
   templateUrl: './general-informations-edit.component.html',
   styleUrl: './general-informations-edit.component.scss'
 })
-export class GeneralInformationsEditComponent implements OnInit {
+export class GeneralInformationsEditComponent implements OnInit, OnChanges {
   passwordStrength: number = 0;
+  countriesListFiltered: CountriesList = [];
 
   @Input({ required: true }) userForm: FormGroup = {} as FormGroup;
+  @Input({ required: true }) countriesList: CountriesList = [];
 
   ngOnInit() { }
+
+  ngOnChanges(changes: SimpleChanges): void { }
 
   get nameControl(): FormControl {
     return this.userForm.get("generalInformations.name") as FormControl;
@@ -41,5 +47,29 @@ export class GeneralInformationsEditComponent implements OnInit {
   onPasswordInputAndChangesEvent(event: Event) {
     const value = (event.target as HTMLInputElement).value;
     this.passwordStrength = passwordStrengthProgressBar(value);
+  }
+
+  onCountryInputEvent(searchTerm: Event) {
+    const countryName = (searchTerm.target as HTMLInputElement).value;
+
+    if (!countryName) return;
+
+    this.onFilterCountriesList(countryName);
+  }
+
+  onCountryFocusEvent() {
+    this.countriesListFiltered = this.countriesList;
+  }
+
+  onCountrySelected(event: MatAutocompleteSelectedEvent) {
+    const searchTerm = event.option.value;
+
+    if (!searchTerm) return;
+  }
+
+  private onFilterCountriesList(searchTerm: string) {
+    const countryName = searchTerm.toLocaleLowerCase().trim();
+    
+    this.countriesListFiltered = this.countriesList.filter(countryResponse => countryResponse.name.toLocaleLowerCase().includes(countryName));
   }
 }
--- userRegistration/src/app/components/user-container/user-container.component.ts
+++ userRegistration/src/app/components/user-container/user-container.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, inject, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { AngularMaterialModule } from '../../angular-material/angular-material.module';
 import { IUser } from '../../interfaces/user/user.interface';
 import { ContactInformationsComponent } from '../contact-informations/contact-informations.component';
@@ -10,6 +10,8 @@ import { UserController } from './user-controller';
 import { ContactInformationsEditComponent } from '../contact-informations-edit/contact-informations-edit.component';
 import { DependentInformationsEditComponent } from '../dependent-informations-edit/dependent-informations-edit.component';
 import { MusicInformationsEditComponent } from '../music-informations-edit/music-informations-edit.component';
+import { CountriesService } from '../../services/countries.service';
+import { CountriesList } from '../../types/countries-list';
 
 @Component({
   selector: 'app-user-container',
@@ -28,11 +30,18 @@ import { MusicInformationsEditComponent } from '../music-informations-edit/music
   templateUrl: './user-container.component.html',
   styleUrl: './user-container.component.scss'
 })
-export class UserContainerComponent extends UserController implements OnChanges {
+export class UserContainerComponent extends UserController implements OnInit, OnChanges {
   currentTabIndex = 0;
+  countriesList: CountriesList = [];
 
   @Input({ required: true }) userSelected: IUser = {} as IUser;
   @Input({ required: true }) userSelectedIndex!: string;
+
+  private readonly _countriesService = inject(CountriesService);
+
+  ngOnInit() {
+    this.getCountries();
+  }
 
   ngOnChanges(changes: SimpleChanges): void {
     const HAS_USER_SELECTED = changes["userSelected"] && Object.keys(changes["userSelected"].currentValue).length > 0;
@@ -40,5 +49,11 @@ export class UserContainerComponent extends UserController implements OnChanges 
     if (HAS_USER_SELECTED) {
       this.fulfillUserForm(this.userSelected);
     }
+  }
+
+  getCountries() {
+    this._countriesService
+      .getCountries()
+      .subscribe(countriesResponse => this.countriesList = countriesResponse);
   }
 }
